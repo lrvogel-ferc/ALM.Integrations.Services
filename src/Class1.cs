f55082117c5d82c4e1615933a67142d7fe76c94c
Lucas Vogel (CTR) <lucas.vogel@ferc.gov>
2025-02-06 14:38:16 -0500

    project structure config

--- src/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace ALM.Integrations.Services;
-
-public class Class1
-{
-
-}
---
+++ src/XrmService.cs
@@ -0,0 +1,11 @@
+﻿namespace ALM.Integrations.Services;
+/// <summary>
+/// The service for managing Dataverse connectivity and queries
+/// </summary>
+public class XrmService
+{
+    public XrmService(IConfiguration configuration)
+    {
+
+    }
+}
